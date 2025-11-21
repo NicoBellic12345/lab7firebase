bd80c00a900a90b462f7edac5ab95bf10b263607
NicoBellic12345 <157093975+NicoBellic12345@users.noreply.github.com>
2025-11-21 14:45:43 +0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "твой-api-key",
+    authDomain: "твой-project.firebaseapp.com",
+    projectId: "твой-project-id",
+    storageBucket: "твой-project.appspot.com",
+    messagingSenderId: "твой-sender-id",
+    appId: "твой-app-id"
+  }
+};
